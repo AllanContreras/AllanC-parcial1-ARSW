201eaab04a72b6e04f610cf7f3e42c79682d0d65
unknown <allan.contreras-r@labinfo.is.escuelaing.edu.co>
2025-09-19 08:19:30 -0500

    subiendo parcial

---
+++ Parcial1-main/src/main/java/edu/eci/arsw/math/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String a[]) {
+        int numThreads = 4;
+
+        System.out.println(bytesToHex(PiDigits.getDigits(0, 10, numThreads)));
+        System.out.println(bytesToHex(PiDigits.getDigits(1, 100, numThreads)));
+        System.out.println(bytesToHex(PiDigits.getDigits(1, 1000000, numThreads)));
+    }
+
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0; j < bytes.length; j++) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < hexChars.length; i = i + 2) {
+            sb.append(hexChars[i + 1]);
+        }
+        return sb.toString();
+    }
+}
---
+++ Parcial1-main/src/main/java/edu/eci/arsw/math/PiDigits.java
@@ -0,0 +1,52 @@
+package edu.eci.arsw.math;
+
+public class PiDigitThread extends Thread {
+    private int start;
+    private int count;
+    private byte[] digits;
+    private int threadId;
+    private static final long SLEEP_INTERVAL = 5000;
+    private static final Object LOCK = new Object();
+
+    public PiDigitThread(int start, int count, int threadId) {
+        this.start = start;
+        this.count = count;
+        this.threadId = threadId;
+        this.digits = new byte[count];
+    }
+
+    @Override
+    public void run() {
+        double sum = 0;
+        int digitsProcessed = 0;
+
+        for (int i = 0; i < count; i++) {
+            if (i % PiDigits.DigitsPerSum == 0) {
+                sum = 4 * PiDigits.sum(1, start)
+                        - 2 * PiDigits.sum(4, start)
+                        - PiDigits.sum(5, start)
+                        - PiDigits.sum(6, start);
+                start += PiDigits.DigitsPerSum;
+            }
+
+            sum = 16 * (sum - Math.floor(sum));
+            digits[i] = (byte) sum;
+            digitsProcessed++;
+
+            if (digitsProcessed % 100 == 0) {
+                synchronized (LOCK) {
+                    try {
+                        System.out.println("Thread " + threadId + " processed " + digitsProcessed + " digits. Press Enter to continue.");
+                        System.in.read();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public byte[] getDigits() {
+        return digits;
+    }
+}
